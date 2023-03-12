15e571af1e7d994802a364454817815451042fc1
arincdemir <68223368+arincdemir@users.noreply.github.com>
2023-03-12 17:17:13 +0300

    added solution to 2008

---
+++ 2008.py
@@ -0,0 +1,24 @@
+from typing import *
+from collections import defaultdict
+class Solution:
+    def maxTaxiEarnings(self, n: int, rides: List[List[int]]) -> int:
+        ridesThatStartAt = defaultdict(list)
+        # create ridesThatStartAt with end and money values
+        for s, e, t in rides:
+            ridesThatStartAt[s].append((e, e - s + t))
+
+        dp = {}
+        def dfs(i):
+            if i == n:
+                return 0
+            
+            if i in dp:
+                return dp[i]
+            
+            # do not take any rides, skip the spot
+            skip = dfs(i + 1)
+
+            # take the rides available
+            take = 0
+            for e, m in ridesThatStartAt[i]:
+
