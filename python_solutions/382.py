1a43fd194a7b632f5c55ab7b0109644529cd979a
arincdemir <68223368+arincdemir@users.noreply.github.com>
2023-03-15 16:49:46 +0300

    382

---
+++ python_solutions/382.py
@@ -0,0 +1,22 @@
+import random
+import math
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+
+    def __init__(self, head: Optional[ListNode]):
+        self.array = []
+        curNode = head
+        while curNode:
+            self.array.append(curNode.val)
+            curNode = curNode.next
+
+    def getRandom(self) -> int:
+        randIndex = math.floor(random.random() * len(self.array))
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()
